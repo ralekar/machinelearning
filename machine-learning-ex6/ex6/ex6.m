282fc33254d395fbe1979018d32437d973fb3c4e
avanti sardesai <avantisardesai@avantis-MacBook-Pro.local>
2016-11-03 06:52:15 -0700

    assignment 6

---
+++ machine-learning-ex6/ex6/ex6.m
@@ -0,0 +1,14 @@
+
+% You will have X, y in your environment
+load('ex6data3.mat');
+
+% Try different SVM Parameters here
+[C, sigma] = dataset3Params(X, y, Xval, yval);
+
+% Train the SVM
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+visualizeBoundary(X, y, model);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
